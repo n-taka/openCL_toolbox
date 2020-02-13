6701d757702a0a5e635122818992de0c00f0649c
Kazutaka Nakashima <kazutaka.nakashima@n-taka.info>
2020-02-14 05:08:45 +0900

    [misc] code cleaning

--- include/openCL_toolbox/openCL_params.h
+++ include/openCL_toolbox/openCL_params.h
@@ -2,6 +2,7 @@
 #define OPENCL_PARAMS_H
 #define CL_HPP_ENABLE_EXCEPTIONS
 #define CL_HPP_TARGET_OPENCL_VERSION 120
+#define CL_HPP_MINIMUM_OPENCL_VERSION 120
 
 #include <CL/cl2.hpp>
 #include <vector>
--- include/openCL_toolbox/openCL_util.h
+++ include/openCL_toolbox/openCL_util.h
@@ -2,6 +2,7 @@
 #define OPENCL_UTIL_H
 #define CL_HPP_ENABLE_EXCEPTIONS
 #define CL_HPP_TARGET_OPENCL_VERSION 120
+#define CL_HPP_MINIMUM_OPENCL_VERSION 120
 
 #include "openCL_params.h"
 
